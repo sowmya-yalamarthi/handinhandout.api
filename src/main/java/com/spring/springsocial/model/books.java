b4408a58430ed73b70877348ed9b0ebe804f8fc3
sowmya-yalamarthi <s542049@nwmissouri.edu>
2021-10-31 22:23:01 -0500

    addingbooksclass

---
+++ src/main/java/com/spring/springsocial/model/books.java
@@ -0,0 +1,10 @@
+package com.spring.springsocial.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+
+public class books {
+
+}
